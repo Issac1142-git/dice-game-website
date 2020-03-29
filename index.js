e5a94b5cdf3d8cd43ff71a91c0f0347f9a33270f
Issac Immanuvel <61177428+Issac1142-git@users.noreply.github.com>
2020-03-30 03:02:05 +0530

    completed Dice Game Website Files

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImg1 = "dice" +randomNumber1+ ".png";
+var srcImg1 = "images/" +randomImg1;
+document.querySelectorAll("img")[0].setAttribute("src", srcImg1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImg2 = "dice" +randomNumber2+ ".png";
+var srcImg2 = "images/" +randomImg2;
+document.querySelectorAll("img")[1].setAttribute("src", srcImg2);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="Player1 wins";
+}
+
+
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player2 wins";
+}
+
+
+else{
+  document.querySelector("h1").innerHTML="Draw";
+
+}
